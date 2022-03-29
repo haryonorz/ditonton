2f65cbdc1e8ffae2af2dc3fd748980f2c6c2b182
haryonorz <45280932+haryonorz@users.noreply.github.com>
2022-03-29 17:08:51 +0700

    Add entities tv show in domain

---
+++ lib/domain/entities/tv_show_detail.dart
@@ -0,0 +1,48 @@
+import 'package:ditonton/domain/entities/genre.dart';
+import 'package:equatable/equatable.dart';
+
+class TvShowDetail extends Equatable {
+  TvShowDetail({
+    required this.backdropPath,
+    required this.firstAirDate,
+    required this.genres,
+    required this.id,
+    required this.name,
+    required this.numberOfEpisodes,
+    required this.numberOfSeasons,
+    required this.originalName,
+    required this.overview,
+    required this.posterPath,
+    required this.voteAverage,
+    required this.voteCount,
+  });
+
+  final String? backdropPath;
+  final String? firstAirDate;
+  final List<Genre> genres;
+  final int id;
+  final String name;
+  final int numberOfEpisodes;
+  final int numberOfSeasons;
+  final String originalName;
+  final String overview;
+  final String posterPath;
+  final double voteAverage;
+  final int voteCount;
+
+  @override
+  List<Object?> get props => [
+        backdropPath,
+        firstAirDate,
+        genres,
+        id,
+        name,
+        numberOfEpisodes,
+        numberOfSeasons,
+        originalName,
+        overview,
+        posterPath,
+        voteAverage,
+        voteCount,
+      ];
+}
