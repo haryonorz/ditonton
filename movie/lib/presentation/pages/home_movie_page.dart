493aa450f769bc07b7aa6aa3d552780c7bc05947
haryonorz <45280932+haryonorz@users.noreply.github.com>
2022-04-24 14:09:54 +0700

    fixed search movie

--- movie/lib/presentation/pages/home_movie_page.dart
+++ movie/lib/presentation/pages/home_movie_page.dart
@@ -1,5 +1,4 @@
 import 'package:core/core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movie/domain/entities/movie.dart';
@@ -37,7 +36,6 @@ class _HomeMoviePageState extends State<HomeMoviePage> {
         actions: [
           IconButton(
             onPressed: () {
-              FirebaseCrashlytics.instance.crash();
               Navigator.pushNamed(context, searchRoute);
             },
             icon: const Icon(Icons.search),
